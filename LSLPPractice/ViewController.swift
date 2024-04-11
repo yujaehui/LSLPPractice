c5e82c7eb8357fdf80503b1869676c088b4a6ea8
Jaehui Yu <pkj0522@naver.com>
2024-04-11 16:15:27 +0900

    Initial Commit

---
+++ LSLPPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSLPPractice
+//
+//  Created by Jaehui Yu on 4/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
